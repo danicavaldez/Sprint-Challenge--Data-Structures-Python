040273f57e358a5bbbaa4c4c62b709178fbbecbf
Danica Valdez <danicaevaldez@gmail.com>
2020-08-02 20:24:53 -0700

    Ring Buffer and Reverse completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,33 @@
+import sys
+sys.path.append('.doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length == self.capacity:
+            if self.storage.tail == self.current:
+                self.current = self.storage.head
+                self.storage.head.value = item
+            else:
+                self.current = self.current.next
+                self.current.value = item
+        else:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
 
     def get(self):
-        pass
+        buffer_list = []
+
+        item = self.storage.head
+
+        while item:
+            buffer_list.append(item.value)
+            item = item.next
+        
+        return buffer_list
+
